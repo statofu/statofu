0d385f877f37a208bf626fcfddda09b5d885a1c6
licg9999 <licg9999@126.com>
2023-04-06 15:14:09 +0800

    build: add dist generation

---
+++ rollup.config.mjs
@@ -0,0 +1,57 @@
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+/**
+ * @typedef {import("rollup").RollupOptionsFunction} RollupOptionsFunction
+ * @typedef {import("rollup").ModuleFormat} ModuleFormat
+ * @typedef {import("rollup").OutputOptions} OutputOptions
+ */
+
+const name = 'statofu';
+const outDir = 'dist';
+const bundle = Boolean(process.env.BUNDLE);
+
+/**
+ * @type {RollupOptionsFunction}
+ */
+function getRollupConfig(cliArgs) {
+  /** @type {ModuleFormat} */
+  const format = cliArgs.format;
+
+  const outputFilename = `${name}.${format}${bundle ? '.bundle' : ''}`;
+
+  /** @type {OutputOptions} */
+  const outputNormal = {
+    file: `${outDir}/${outputFilename}.js`,
+    format,
+    name,
+    sourcemap: true,
+  };
+
+  /** @type {OutputOptions} */
+  const outputCompressed = {
+    ...outputNormal,
+    file: `${outDir}/${outputFilename}.min.js`,
+    plugins: [
+      terser({
+        format: { comments: false },
+      }),
+    ],
+  };
+
+  return {
+    input: 'src/index.ts',
+    output: [outputNormal, outputCompressed],
+    plugins: [
+      bundle && nodeResolve(),
+      bundle && commonjs(),
+      typescript({
+        tsconfig: 'additional-configs/tsconfig.rollup.json',
+      }),
+    ].filter(Boolean),
+  };
+}
+
+export default getRollupConfig;
